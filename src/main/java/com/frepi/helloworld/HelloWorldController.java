617bcc8b9066069df75107fe6c42b770bfbb948b
anim <anmkash@gmail.com>
2024-02-10 14:34:23 +0900

    Add: path-variable based API definition.

--- src/main/java/com/frepi/helloworld/HelloWorldController.java
+++ src/main/java/com/frepi/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.frepi.helloworld;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @RestController
@@ -11,4 +12,11 @@ public class HelloWorldController {
     public HelloWorld helloWorld() {
         return new HelloWorld("from Anim");
     }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorld helloWroldWithPathVariable(@PathVariable String name) {
+        return new HelloWorld(String.format("Hello World, %s", name));
+    }
+
+
 }
